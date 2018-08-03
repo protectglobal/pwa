2014b014b6e7eb2720fa240ff5fd07cbb408a41c
fede-rodes <rodesf@gmail.com>
2018-08-03 13:22:51 +0200

    adding global data provider

--- client/src/pages/home-page/index.js
+++ client/src/pages/home-page/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { propType } from 'graphql-anywhere';
 import styled from 'styled-components';
+import userFragment from '../../graphql/user/fragment/user';
 import EventsList from '../../components/events-list';
 import ClearEventsBtn from '../../components/clear-events-btn';
 
@@ -12,13 +14,22 @@ const Title = styled.h3`
 //------------------------------------------------------------------------------
 // COMPONENT:
 //------------------------------------------------------------------------------
-const HomePage = () => (
+const HomePage = ({ curUser }) => (
   <div>
     <Title>PWA</Title>
+    <div>{JSON.stringify(curUser, { indent: true })}</div>
     <h3>Events List - <ClearEventsBtn /></h3>
     <EventsList />
     <div className="mb2" />
   </div>
 );
+
+HomePage.propTypes = {
+  curUser: propType(userFragment), // eslint-disable-line
+};
+
+HomePage.defaultProps = {
+  curUser: null,
+};
 
 export default HomePage;
