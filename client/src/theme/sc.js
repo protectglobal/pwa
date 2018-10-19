21f8629451a1f49d38655b21f826fd8e3264802d
fede-rodes <rodesf@gmail.com>
2018-10-19 18:24:51 +0200

    adding active prop to SquareButton

--- client/src/theme/sc.js
+++ client/src/theme/sc.js
@@ -3,6 +3,7 @@ const theme = {
   color: {
     default: 'rgb(129, 129, 130)',
     primary: 'rgb(0, 111, 180)',
+    primary20: 'rgb(0, 111, 180, 0.2)',
     danger: 'tomato',
     dangerLight: '#fcdbd9',
     success: 'green',
@@ -10,6 +11,7 @@ const theme = {
     link: 'rgb(0, 111, 180)',
     black: 'rgb(26, 23, 27)',
     greyLight: 'rgb(236, 236, 236)',
+    white: 'white',
   },
   fontSize: {
     small: '14px',
